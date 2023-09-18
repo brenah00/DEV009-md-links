dcf3ff8700456b42b97124c79ee7992d18edc75b
Brenda Aguilar <blenda916@gmail.com>
2023-09-18 11:53:51 -0600

    ejecucion con comando mdlinks

--- cli.js
+++ cli.js
@@ -1,12 +1,10 @@
+#!/usr/bin/env node
+require('colors');
 const process = require('process');
 const mdlinks = require('./index.js');
 const {stats} = require('./fileInformation.js');
 const path = process.argv[2];
 const options = process.argv.slice(3);
-
-/* console.log(process.argv);
-console.log(path)
-console.log(options) */
 if(options.length === 0){
   mdlinks(path)
   .then((links) => {
@@ -18,14 +16,12 @@ if(options.length === 0){
 } else{
   mdlinks(path, true)
   .then((links) => {
-    //console.log(options);
-    /*if(!options.includes('--validate') || !options.includes('--stats')){
-        console.log('Introduce opciones validas. ', options.filter(option => {
-            return option !== '--validate' && option !== '--stats';
-        }), 'no existe');
-    } else*/ /* if(options.includes('--validate') && options.includes('--stats')){
-      console.log(stats(links, true));
-    } else*/ if(options.includes('--stats')){
+    const aux = options.filter(option => {
+        return option !== '--validate' && option !== '--stats';
+    })
+    if(aux.length > 0){
+        console.log(`Introduce opciones validas. ${aux} no existe`);
+    } else if(options.includes('--stats')){
       console.log(stats(links, options.includes('--validate')));
     } else if(options.includes('--validate')){
       console.log(links);
