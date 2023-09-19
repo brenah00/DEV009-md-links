0205ba9d75662bf268e10c6cf6946f7fe0a339de
Brenda Aguilar <blenda916@gmail.com>
2023-09-19 00:55:32 -0600

    Tests de hito 5 - busqueda de enlaces en subcarpetas y función stats

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,5 @@
 const mdlinks = require('../index.js');
+const { stats } = require('../fileInformation.js')
 
 describe('mdlinks', () => {
   it('Debería rechazar con un error -No existen enlaces, intenta con otro archivo markdown.-', () => {
@@ -10,10 +11,10 @@ describe('mdlinks', () => {
   it('Debería rechazar con un error -El archivo markdown o directorio no existe.-', () => {
     expect(mdlinks('dontExist.md', false)).rejects.toMatch('El archivo markdown o directorio no existe.');
   });
-  it('Debería resolver con un arreglo de 6 enlaces cuando el argumento es un directorio', () => {
-    return mdlinks('./test/testing-files', true).then((links) => {
+  it('Debería resolver con un arreglo de 2 enlaces con status y request definidos', () => {
+    return mdlinks('./test/testing-files/README1.md', true).then((links) => {
       expect(Array.isArray(links)).toBe(true);
-      expect(links.length).toBe(6);
+      expect(links.length).toBe(2);
       links.forEach((link) => {
         expect(link.status).toBeDefined();
         expect(link.request).toBeDefined();
@@ -30,13 +31,51 @@ describe('mdlinks', () => {
       });
     });
   });
-  it('Debería resolver con un arreglo de 2 enlaces', () => {
+  it('Debería resolver con un arreglo de 6 enlaces cuando el argumento es el directorio ./test/testing-files', () => {
     return mdlinks('./test/testing-files', true).then((links) => {
       expect(Array.isArray(links)).toBe(true);
-      links.forEach((link) => {
-        expect(link.status).toBeDefined();
-        expect(link.request).toBeDefined();
-      });
+      expect(links.length).toBe(6);
     });
+  });
+});
+describe('stats', () => {
+  const links = [{
+    href: 'https://test.com.mx',
+    text: 'md-links',
+    file: 'C:\\Users\\LaboratorianX\\Laboratoria\\DEV009-md-links\\testing-file.text',
+    status: 200,
+    request: 'ok'
+  },
+  {
+    href: 'https://test1.com.mx',
+    text: 'Arreglos',
+    file: 'C:\\Users\\LaboratorianX\\Laboratoria\\DEV009-md-links\\testing-file.text',
+    status: 200,
+    request: 'ok'
+  },
+  {
+    href: 'https://test2.com.mx',
+    text: 'md-links-test',
+    file: 'C:\\Users\\LaboratorianX\\Laboratoria\\DEV009-md-links\\testing-file.text',
+    status: 404,
+    request: 'fail'
+  }];
+
+  it('Debería retornar un objeto con Total = 3 e Unique = 3, OK y Broken no deben estar definidos', () => {
+    const linkStats = stats(links);
+    // console.log(linkStats)
+    expect(linkStats.Total).toBe(3);
+    expect(linkStats.Unique).toBe(3);
+    expect(linkStats.OK).not.toBeDefined();
+    expect(linkStats.Broken).not.toBeDefined();
+  });
+  it('Debería retornar un objeto con OK = 2 y Broken = 1', () => {
+    const linkStats = stats(links, true);
+     // console.log(linkStats)
+    expect(linkStats.OK).toBeDefined();
+    expect(linkStats.Broken).toBeDefined();
+
+    expect(linkStats.OK).toBe(2);
+    expect(linkStats.Broken).toBe(1);
   });
 });
