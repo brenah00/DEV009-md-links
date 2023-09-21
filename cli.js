a0843107b0611d68fef0204dfa525f1dad456c7b
Brenda Aguilar <blenda916@gmail.com>
2023-09-21 10:19:26 -0600

    test y readme

--- cli.js
+++ cli.js
@@ -5,29 +5,20 @@ const mdlinks = require('./index.js');
 const {stats} = require('./fileInformation.js');
 const path = process.argv[2];
 const options = process.argv.slice(3);
-if(options.length === 0){
-  mdlinks(path)
-  .then((links) => {
-    console.log(links);
-  })
-  .catch((error) => {
-    console.log(error);
-  }); 
-} else{
-  mdlinks(path, true)
+
+mdlinks(path, options.includes('--validate'))
   .then((links) => {
     const aux = options.filter(option => {
-        return option !== '--validate' && option !== '--stats';
+      return option !== '--validate' && option !== '--stats';
     })
-    if(aux.length > 0){
-        console.log(`Introduce opciones validas. ${aux} no existe`);
-    } else if(options.includes('--stats')){
+    if(aux.length > 0) {
+      console.log(`${aux} no es una opción válida.`);
+    } else if(options.includes('--stats')) {
       console.log(stats(links, options.includes('--validate')));
-    } else if(options.includes('--validate')){
+    } else {
       console.log(links);
     }
   })
   .catch((error) => {
     console.log(error);
-  });
-}
+  }); 
